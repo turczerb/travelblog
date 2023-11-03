b6df7f5bed7f3e001c1ee39c61110685e697aedd
turczerb <turczerb@gmail.com>
2023-11-03 18:44:54 +0100

    destination works

--- frontend/src/components/Post/CreateNewPost.jsx
+++ frontend/src/components/Post/CreateNewPost.jsx
@@ -3,13 +3,14 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useState } from "react";
 import { NavbarData } from "../NavBar/NavBarData";
-import Multiselect from "multiselect-react-dropdown";
+import Select from "react-select";
 
 const Container = styled.div`
   display: flex;
   box-sizing: border-box;
   align-items: center;
   justify-content: center;
+  margin-bottom: 120px;
 `;
 
 const Form = styled.form`
@@ -22,11 +23,44 @@ const UListi = styled.ul`
   list-style: none;
 `;
 
+const options = [
+  { value: "London", label: "LONDON" },
+  { value: "x", label: "bigyó" },
+  { value: "v", label: "mogyó" },
+];
+
+const options2 = NavbarData.map((item, i) => {
+  if (item.title === "Destinations") {
+    return (
+      <>
+        {item.subNav.map((desti, index) => {
+          return {
+            label: desti.title,
+            value: desti,
+            key: index,
+          };
+        })}
+      </>
+    );
+  }
+});
+
 const CreateNewPost = () => {
-  const [placeChange, setPlaceChange] = useState("Select a location");
+  const [placeChange, setPlaceChange] = useState("");
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
   const handlePlaceChange = (e) => {
     setPlaceChange(e.target.value);
+  };
+
+  //ez miért van így
+  const handleChoose = (selectedOption) => {
+    setSelectedOptions(selectedOption);
+  };
+
+  const submit = () => {
+    console.log(placeChange);
+    console.log(selectedOptions);
   };
 
   return (
@@ -35,14 +69,23 @@ const CreateNewPost = () => {
         <input type="title" placeholder={"Title"} />
         <input type="summary" placeholder={"Summary"} />
         <input type="file" />
-        <select onChange={handlePlaceChange} placeholder="select one!">
-          {NavbarData.map((item, index) => {
+        <select
+          onChange={handlePlaceChange}
+          placeholder="select one!"
+          required
+          value={placeChange}
+        >
+          <option value="" disabled selected>
+            Choose your destination
+          </option>
+          {NavbarData.map((item, i) => {
             if (item.title === "Destinations") {
               return (
                 <>
+                  <div key={item.id}></div>
                   {item.subNav.map((desti, index) => {
                     return (
-                      <option value={desti.title} item={desti} key={index}>
+                      <option item={desti} key={index}>
                         {desti.title}
                       </option>
                     );
@@ -53,8 +96,15 @@ const CreateNewPost = () => {
           })}
         </select>
 
+        <Select
+          options={options}
+          value={selectedOptions}
+          onChange={handleChoose}
+          isMulti={true}
+        ></Select>
         <ReactQuill />
       </Form>
+      <button onClick={submit}>klikk</button>
     </Container>
   );
 };
